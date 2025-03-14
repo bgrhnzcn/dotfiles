905f557f94e13f5e3b87b6b8002be9a19cd7da37
bgrhnzcn <bugrahanozcan16@gmail.com>
2025-03-15 00:34:11 +0300

    Added Python Virtual Environment support for bash prompt. Made some improvement to make easier changes in the future.

--- bash_themes.sh
+++ bash_themes.sh
@@ -39,8 +39,8 @@ set_theme()
 	git_err2="${palette[11]}"
 	exit1="${palette[12]}"
 	exit2="${palette[13]}"
-	py1="${palette[12]}"
-	py2="${palette[13]}"
+	py1="${palette[2]}"
+	py2="${palette[3]}"
 }
 
 update_ps1()
@@ -62,6 +62,7 @@ update_ps1()
 	set_theme
 	local branch_name=$(git branch --show-current 2>/dev/null)
 	local changes=$(git diff --name-only 2>/dev/null | wc -l 2>/dev/null)
+	
 	if [ "$changes" == "0" ]; then
 		git1=$git1
 		git2=$git2
@@ -69,29 +70,42 @@ update_ps1()
 		git1=$git_err1
 		git2=$git_err2
 	fi
-	exit_prompt=""
+	
+	local shell_prompt=" ${shell1}${circle_left}${shell2}${shell_ico}  ${shell}${shell1}${circle_right}\e[0m "
+
+	local user_prompt="${user1}${circle_left}${user2}${user_ico}  ${USER}${user1}${circle_right}\e[0m "
+
+	local dir_prompt="${dir1}${circle_left}${dir2}${dir_ico}  ${dir}${dir1}${circle_right}\e[0m "
+
+	local git_prompt="${git1}${circle_left}${git2}${git_ico} ${branch_name}${git1}${circle_right}\e[0m "
+
+	local exit_prompt=""
 	if [ $exit != "0" ]; then
-		exit_prompt="${exit1}${circle_left}${exit2}${exit_ico} ${exit}${exit1}${circle_right}\e[0m"
+		exit_prompt="${exit1}${circle_left}${exit2}${exit_ico} ${exit}${exit1}${circle_right}\e[0m "
 	fi
-	local right="${exit_prompt} ${clock1}${circle_left}${clock2}${clock_ico}  ${clock}${clock1}${circle_right}\e[0m"
+	
+	local python_env_prompt=""
+	if [ -n "$VIRTUAL_ENV" ]; then
+		python_env_prompt="${py1}${circle_left}${py2}${python_ico} ${py1}${circle_right}\e[0m "
+	fi
+
+	local right="${python_env_prompt}${exit_prompt}${clock1}${circle_left}${clock2}${clock_ico} ${clock}${clock1}${circle_right}\e[0m"
+	
 	if [ -z "$branch_name" ]; then
 		# No branch, not in a git repo
-		local left=" ${shell1}${circle_left}${shell2}${shell_ico}  ${shell}${shell1}${circle_right}\e[0m \
-${user1}${circle_left}${user2}${user_ico}  ${USER}${user1}${circle_right}\e[0m \
-${dir1}${circle_left}${dir2}${dir_ico}  ${dir}${dir1}${circle_right}\e[0m"
+		local left="${shell_prompt}${user_prompt}${dir_prompt}"
 	else
 		# Branch exist, print that branch and change color to indicate state.
-		local left=" ${shell1}${circle_left}${shell2}${shell_ico}  ${shell}${shell1}${circle_right}\e[0m \
-${user1}${circle_left}${user2}${user_ico}  ${USER}${user1}${circle_right}\e[0m \
-${git1}${circle_left}${git2}${git_ico} ${branch_name}${git1}${circle_right}\e[0m \
-${dir1}${circle_left}${dir2}${dir_ico}  ${dir}${dir1}${circle_right}\e[0m"
+		local left="${shell_prompt}${user_prompt}${git_prompt}${dir_prompt}"
 	fi
+
 	# Get column count to calculate prompt size
 	local columns=$(tput cols)
 	local plain_left=$(echo -e "${left}" | sed -r 's/\x1B\[[0-9;]*[a-zA-Z]//g')
 	local plain_right=$(echo -e "${right}" | sed -r 's/\x1B\[[0-9;]*[a-zA-Z]//g')
 	local space_count=$((columns - ${#plain_left} - ${#plain_right}))
 	local spaces=$(printf '%*s' ${space_count})
+	
 	PS1=$(printf "$left$spaces$right\n  \uF101 ")
 	PS2=$(printf "  \uF101 ")
 	PS2="  \uF101 "
