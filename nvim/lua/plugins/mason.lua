01061a6140868ffe74a304d10b68277d6d2adb30
bgrhnzcn <bugrahanozcan16@gmail.com>
2025-04-23 11:54:03 +0300

    .

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "cmake", "rust_analyzer", }
+				ensure_installed = { "lua_ls", "cmake", "rust_analyzer", }
 			})
 		end
 	},
@@ -21,10 +21,17 @@ return {
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.clangd.setup({ capabilities = capabilities })
 			lspconfig.cmake.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities})
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 			vim.keymap.set('n', '<C-i>', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
 			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = true,
+				severity_sort = true,
+			})
 		end
 	}
 }
