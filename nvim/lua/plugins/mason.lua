bd90759c08a51c23b03b25025d3a2f94a1b5fe41
bgrhnzcn <bgrhnzcn@gmail.com>
2025-11-07 14:45:55 +0300

    nvim updated. Now works without errors.

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -8,20 +8,20 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "cmake", "rust_analyzer", }
+				ensure_installed = { "clangd", "lua_ls", "cmake", "bashls"},
+				handlers = {
+					function(server_name)
+						server_name.setup({ capabilities = capabilities })
+					end
+				}
 			})
 		end
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.cmake.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 			vim.keymap.set('n', '<C-i>', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
 			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
@@ -31,6 +31,21 @@ return {
 				underline = true,
 				update_in_insert = true,
 				severity_sort = true,
+			})
+
+			local max_width = math.max(math.floor(vim.o.columns * 0.7), 100)
+			local max_height = math.max(math.floor(vim.o.lines * 0.3), 30)
+
+			-- NOTE: the hover handler returns the bufnr,winnr so can be used for mappings
+			vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+			  vim.lsp.handlers.hover,
+			  { boarder = 'rounded', max_width = max_width, max_height = max_height }
+			)
+
+			vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+			  border = 'rounded',
+			  max_width = max_width,
+			  max_height = max_height,
 			})
 		end
 	}
